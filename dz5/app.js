acc16991d7a9def25af15ba488a8f208495dcc06
Mark Volpov <markvolpov.98@gmail.com>
2023-08-15 18:41:04 +0300

    fixed

--- dz5/app.js
+++ dz5/app.js
@@ -30,32 +30,30 @@ if (userAge && userCity && userFavSport) {
         case "football":
             favSport = "Great choise! You wanna be as Cristiano Ronaldo?";
             break;
-            case "Football":
-                favSport = "Great choise! You wanna be as Cristiano Ronaldo?";
-                break;
-                case "boxing":
-                    favSport = "Great choise! You wanna be as Floyd Mayweather Jr. ?";
-                    break;
-                    case "Boxing":
-                        favSport = "Great choise! You wanna be as Floyd Mayweather Jr. ?";
-                        break;
-                        case "formula one":
-                            favSport = "Great choise! You want to be as Michael Schumacher ? ";
-                            break;
-                            case "formula 1":
-                                favSport = "Great choise! You want to be as Michael Schumacher ? ";
-                                break;
-                                case "Formula one":
-                                    favSport = "Great choise! You want to be as Michael Schumacher ? ";
-                                    break;
-                                    case "Formula 1":
-                                        favSport = "Great choise! You want to be as Michael Schumacher ? ";
-                                        break;
-                                        
-                                        
-                                        default:
-                                            favSport = ("Good sport choise");
-                                        };
+        case "Football":
+            favSport = "Great choise! You wanna be as Cristiano Ronaldo?";
+            break;
+        case "boxing":
+            favSport = "Great choise! You wanna be as Floyd Mayweather Jr. ?";
+            break;
+        case "Boxing":
+            favSport = "Great choise! You wanna be as Floyd Mayweather Jr. ?";
+            break;
+        case "formula one":
+            favSport = "Great choise! You want to be as Michael Schumacher ? ";
+            break;
+        case "formula 1":
+            favSport = "Great choise! You want to be as Michael Schumacher ? ";
+            break;
+        case "Formula one":
+            favSport = "Great choise! You want to be as Michael Schumacher ? ";
+            break;
+        case "Formula 1":
+            favSport = "Great choise! You want to be as Michael Schumacher ? ";
+            break;
+        default:
+            favSport = ("Good sport choise");
+        };
                                         
                                     
     alert(`Your age is ${age}, \n${city}, \n${favSport}`);
