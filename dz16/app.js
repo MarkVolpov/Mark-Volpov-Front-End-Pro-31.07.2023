e9deba1823c30ee1b8df1864260fad7b77b0e56c
Mark Volpov <markvolpov.98@gmail.com>
2023-09-15 18:42:26 +0300

    freeze deleted

--- dz16/app.js
+++ dz16/app.js
@@ -1,66 +1,57 @@
-
-
-
-
-
 const Roles = {};
 
-Object.defineProperties(Roles , {
-    Admin: {
-        value: 'admin',
-        writable: false,
-        enumerable: false,
-        configurable: false,
+Object.defineProperties(Roles, {
+  Admin: {
+    value: "admin",
+    writable: false,
+    enumerable: false,
+    configurable: false,
+  },
+  Editor: {
+    value: "editor",
+    writable: false,
+    enumerable: true,
+    configurable: false,
+  },
+  Reader: {
+    value: "reader",
+    writable: false,
+    enumerable: true,
+    configurable: false,
+  },
+  SuperAdmin: {
+    value: "super admin",
+    writable: false,
+    enumerable: false,
+    configurable: false,
+  },
+});
 
-    },
-    Editor: {
-        value: 'editor',
-        writable: false,
-        enumerable: true,
-        configurable: false,
-    },
-    Reader: {
-        value: 'reader',
-        writable: false,
-        enumerable:true,
-        configurable: false,
-    },
-    SuperAdmin: {
-        value: 'super admin',
-        writable: false,
-        enumerable: false,
-        configurable: false,
-    },
-
-})
-
-Object.freeze(Roles)
 
 
 class User {
-    constructor(id,name,lastname,role) {
-        this.id = id;
-        this.name = name;
-        this.lastname = lastname;
-        this.role = role;
-        Object.defineProperty(this,'role', {
-            value: role,
-            writable:false,
-            enumerable: false,
-            configurable: false,
-        });
-    }
-   
+  constructor(id, name, lastname, role) {
+    this.id = id;
+    this.name = name;
+    this.lastname = lastname;
+    this.role = role;
+    Object.defineProperty(this, "role", {
+      value: role,
+      writable: false,
+      enumerable: false,
+      configurable: false,
+    });
+  }
 }
 delete Roles.Reader; // doesn't work
 console.log(Roles.Admin); // 'admin';
-Roles.Admin = 'hello'; // doesn't change this property
-Roles.newProp = 'value'; // doesn't change Role object
+Roles.Admin = "hello"; // doesn't change this property
+Roles.newProp = "value"; // doesn't change Role object
 
-console.log(Roles)
-const editor = new User(1, 'John', 'Doe', Roles.Admin);
+console.log(Roles);
+const editor = new User(1, "John", "Doe", Roles.Admin);
 console.log(editor.role); // 'admin';
 editor.role === Roles.Admin; // true;
 editor.role = false; // doesn't do anything
 delete editor.role; // doesn't do anything
-console.log(editor)
+console.log(editor);
