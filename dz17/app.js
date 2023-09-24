1af50bcd961be6bf069a520b00e9f26846b537fc
Mark Volpov <markvolpov.98@gmail.com>
2023-09-24 20:55:37 +0300

    StopWatch

---
+++ dz17/app.js
@@ -0,0 +1,53 @@
+class StopWatch {
+    constructor(callback) {
+      this.callback = callback;
+      this.running = false;
+      this.time = { hours: 0, minutes: 0, seconds: 0 };
+      this.interval = null;
+    }
+  
+    start() {
+      if (!this.running) {
+        this.running = true;
+        this.interval = setInterval(() => {
+          this.time.seconds++;
+          if (this.time.seconds === 60) {
+            this.time.seconds = 0;
+            this.time.minutes++;
+            if (this.time.minutes === 60) {
+              this.time.minutes = 0;
+              this.time.hours++;
+            }
+          }
+          this.callback(this.time);
+        }, 1000);
+      }
+    }
+  
+    pause() {
+      if (this.running) {
+        clearInterval(this.interval);
+        this.running = false;
+      }
+    }
+  
+    reset() {
+      this.pause();
+      this.time = { hours: 0, minutes: 0, seconds: 0 };
+    }
+  
+    getTime() {
+      return this.time ;
+    }
+  }
+  
+  const TickTuck = (time) => console.log(time);
+  const stopWatch = new StopWatch(TickTuck);
+  stopWatch.start();
+  
+
+setTimeout(() => {
+  stopWatch.reset();
+},10000);
+
+
