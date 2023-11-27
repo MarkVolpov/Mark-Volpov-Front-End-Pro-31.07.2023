15f50b5cba7a9ccb8851e702d5010ae3a17cb1d5
Mark Volpov <markvolpov.98@gmail.com>
2023-11-27 21:58:09 +0200

    fixed 28' hw

--- dz ajax/app.js
+++ dz ajax/app.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var xhr = new XMLHttpRequest();
-    var apiKey = "5d066958a60d315387d9492393935c19";
-    var city = "LVIV";
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + apiKey;
+    let xhr = new XMLHttpRequest();
+    let apiKey = "5d066958a60d315387d9492393935c19";
+    let city = "LVIV";
+    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + apiKey;
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            var weatherData = JSON.parse(xhr.responseText);
+            let weatherData = JSON.parse(xhr.responseText);
             displayWeather(weatherData);
         }
     };
@@ -23,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("windSpeed").innerText = data.wind.speed;
         document.getElementById("windDirection").innerText = data.wind.deg;
 
-        var iconCode = data.weather[0].icon;
-        var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        let iconCode = data.weather[0].icon;
+        let iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
         document.getElementById("weatherIcon").src = iconUrl;
     }
 });
